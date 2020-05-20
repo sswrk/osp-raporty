b773ca2dcba9cffeadf39d6be95888e3bd308eb4
sswrk <58403334+sswrk@users.noreply.github.com>
2020-05-21 00:59:05 +0200

    login zaczalem

--- pc-demo/main.py
+++ pc-demo/main.py
@@ -1,26 +1,26 @@
-from kivy.properties import StringProperty
-
 from MainMenu import MainMenu
-from ReportChoiceScreen import ReportChoiceScreen
 from ReportScreen import ReportScreen
-from MonthlyReportScreen import MonthlyReportScreen
-from YearlyReportScreen import YearlyReportScreen
-from datetime import date
+from Login import CreateAccountWindow
+from Login import LoginWindow
 
 import kivy
-kivy.require('1.1.1')  # replace with your current kivy version !
+kivy.require('1.1.1')
 
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
 Window.clearcolor = (0, 0.5, 1, 0)
 
 sm = ScreenManager()
+sm.add_widget(LoginWindow(name='login'))
 sm.add_widget(MainMenu(name='menu'))
-#sm.add_widget(ReportChoiceScreen(name='choice'))
 sm.add_widget(ReportScreen(name='report'))
-#sm.add_widget(MonthlyReportScreen(name='monthly'))
-#sm.add_widget(YearlyReportScreen(name='yearly'))
+sm.add_widget(CreateAccountWindow(name='create'))
+
+sm.current = 'login'
+
+def getScreenManager():
+    return sm
 
 class MainApp(App):
     def build(self):
