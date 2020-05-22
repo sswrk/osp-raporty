02475edeeea1ffdfe6c47b5d31357d42fc76428c
sswrk <58403334+sswrk@users.noreply.github.com>
2020-05-22 04:06:58 +0200

    cleanup

--- ReportChoiceScreen.py
+++
@@ -1,8 +0,0 @@
-from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
-with open('reportchoicescreen.kv', encoding='utf8') as f:
-    Builder.load_string(f.read())
-
-class ReportChoiceScreen(Screen):
-    pass
