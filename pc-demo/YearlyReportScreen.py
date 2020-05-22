248a4474608f0cac559b187674150f5effbd3d81
sswrk <58403334+sswrk@users.noreply.github.com>
2020-05-22 04:11:05 +0200

    cleanup

--- pc-demo/YearlyReportScreen.py
+++
@@ -1,8 +0,0 @@
-from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
-with open('yearlyreportscreen.kv', encoding='utf8') as f:
-    Builder.load_string(f.read())
-
-class YearlyReportScreen(Screen):
-    pass
