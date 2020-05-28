0bd1622ac381392b9686edafe594690e71608ece
sswrk <58403334+sswrk@users.noreply.github.com>
2020-05-28 16:57:36 +0200

    dd

--- pc-demo/FirebaseAuth.py
+++ pc-demo/FirebaseAuth.py
@@ -49,125 +49,102 @@ from RegisterScreen import RegisterScreen
 
 
 class FirebaseAuth(Screen, EventDispatcher):
-    web_api_key = StringProperty()
+    auth_key = StringProperty()
 
     refresh_token = ""
-    localId = ""
-    idToken = ""
+    logged_in = BooleanProperty(False)
 
-    login_success = BooleanProperty(False)
-    sign_up_msg = StringProperty()
-    sign_in_msg = StringProperty()
-    email_exists = BooleanProperty(False)
-    email_not_found = BooleanProperty(False)
-
-    def on_web_api_key(self, *args):
+    def __init__(self, **kw):
+        super().__init__(**kw)
         self.refresh_token_file = App.get_running_app().user_data_dir + "/refresh_token.txt"
+
+    def on_auth_key(self, *args):
         if os.path.exists(self.refresh_token_file):
-            self.load_saved_account()
+            self.reload_user()
 
     def sign_up(self, email, password):
-        signup_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + self.web_api_key
-        signup_payload = {"email": email, "password": password, "returnSecureToken": "true"}
-        result = requests.post(signup_url, json=signup_payload)
-        is_login_successful = result.ok
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + self.auth_key
+        payload = {"email": email, "password": password, "returnSecureToken": "true"}
+        result = requests.post(url, json=payload)
         json_result = result.json()
-        if (is_login_successful):
-            self.successful_login(signup_url, json_result)
+        if result.ok:
+            self.login_success(json_result)
         else:
-            self.sign_up_failure(signup_url, json_result)
+            self.sign_up_failure(json_result)
 
-    def successful_login(self, urlrequest, log_in_data):
+    def login_success(self, log_in_data):
         self.refresh_token = log_in_data['refreshToken']
         ConnectionInfo.uid = log_in_data['localId']
-        self.idToken = log_in_data['idToken']
-        self.save_refresh_token(self.refresh_token)
-        self.login_success = True
+        with open(self.refresh_token_file, "w") as f:
+            f.write(self.refresh_token)
+        self.logged_in = True
         self.load_reports()
 
-    def sign_up_failure(self, urlrequest, failure_data):
-        self.email_exists = False
+    def sign_up_failure(self, failure_data):
         msg = failure_data['error']['message'].replace("_", " ").capitalize()
-        self.sign_up_msg = msg
-        if msg == "Email exists":
-            self.email_exists = True
         pop = Popup(title='Nie udało się zarejestrować',
                     content=Label(text=msg),
                     size_hint=(None, None), size=(400, 400))
         pop.open()
 
     def sign_in(self, email, password):
-        sign_in_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + self.web_api_key
-        sign_in_payload = {"email": email, "password": password, "returnSecureToken": True}
-        result = requests.post(sign_in_url, json=sign_in_payload)
-        is_login_successful = result.ok
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + self.auth_key
+        payload = {"email": email, "password": password, "returnSecureToken": True}
+        result = requests.post(url, json=payload)
         json_result = result.json()
-        if (is_login_successful):
-            self.successful_login(sign_in_url, json_result)
+        if result.ok:
+            self.login_success(json_result)
         else:
-            self.sign_in_failure(sign_in_url, json_result)
+            self.sign_in_failure(json_result)
 
-    def sign_in_failure(self, urlrequest, failure_data):
-        self.email_not_found = False
+    def sign_in_failure(self, failure_data):
         msg = failure_data['error']['message'].replace("_", " ").capitalize()
-        self.sign_in_msg = msg
-        if msg == "Email not found":
-            self.email_not_found = True
         pop = Popup(title='Nie udało się zalogować',
                     content=Label(text=msg),
                     size_hint=(None, None), size=(400, 400))
         pop.open()
 
     def reset_password(self, email):
-        reset_pw_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key=" + self.web_api_key
-        reset_pw_data = {"email": email, "requestType": "PASSWORD_RESET"}
-        result = requests.post(reset_pw_url, json=reset_pw_data)
-        is_login_successful = result.ok
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key=" + self.auth_key
+        payload = {"email": email, "requestType": "PASSWORD_RESET"}
+        result = requests.post(url, json=payload)
         json_result = result.json()
-        if (is_login_successful):
-            self.successful_reset(reset_pw_url, json_result)
+        if result.ok:
+            self.reset_success()
         else:
-            self.sign_in_failure(reset_pw_url, json_result)
+            self.sign_in_failure(json_result)
 
-    def successful_reset(self, urlrequest, reset_data):
-        self.sign_in_msg = "Wysłano e-mail z dalszymi instrukcjami"
+    def reset_success(self):
+        msg = "Wysłano e-mail z dalszymi instrukcjami"
         pop = Popup(title='Reset hasła',
-                    content=Label(text=self.sign_in_msg),
+                    content=Label(text=msg),
                     size_hint=(None, None), size=(400, 400))
         pop.open()
 
-    def save_refresh_token(self, refresh_token):
-        with open(self.refresh_token_file, "w") as f:
-            f.write(refresh_token)
-
-    def load_refresh_token(self):
+    def reload_user(self):
         with open(self.refresh_token_file, "r") as f:
             self.refresh_token = f.read()
-
-    def load_saved_account(self):
-        self.load_refresh_token()
-        refresh_url = "https://securetoken.googleapis.com/v1/token?key=" + self.web_api_key
+        refresh_url = "https://securetoken.googleapis.com/v1/token?key=" + self.auth_key
         refresh_payload = dumps({"grant_type": "refresh_token", "refresh_token": self.refresh_token})
         UrlRequest(refresh_url, req_body=refresh_payload,
-                   on_success=self.successful_account_load,
+                   on_success=self.user_reload_success,
                    ca_file=certifi.where(), verify=True)
 
-    def successful_account_load(self, urlrequest, loaded_data):
-        self.idToken = loaded_data['id_token']
+    def user_reload_success(self, url, loaded_data):
         ConnectionInfo.uid = loaded_data['user_id']
-        self.login_success = True
+        self.logged_in = True
         self.load_reports()
 
     def sign_out(self):
-        with open(self.refresh_token_file, 'w') as f:
-            f.write("")
-        self.login_success = False
+        with open(self.refresh_token_file, 'w') as rf:
+            rf.write("")
+        self.logged_in = False
 
     def load_reports(self):
         url = ConnectionInfo.database_url + ConnectionInfo.uid + '/.json'
         ConnectionInfo.reports = json.loads(requests.get(url + '?auth=' + ConnectionInfo.database_auth_key).content)
         self.parent.ids['report_list'].ids['reports_list_grid'].clear_widgets()
-        if (ConnectionInfo.reports):
+        if ConnectionInfo.reports:
             for report in ConnectionInfo.reports:
                 label = ReportLabel(report=report)
                 grid = self.parent.ids['report_list'].ids['reports_list_grid']
