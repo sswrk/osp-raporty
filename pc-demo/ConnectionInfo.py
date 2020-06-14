60786bfe27409b99e321d66b99eaae5f78857cff
sswrk <58403334+sswrk@users.noreply.github.com>
2020-06-14 19:36:35 +0200

    czyszczenie ze smieci

--- pc-demo/ConnectionInfo.py
+++
@@ -1,9 +0,0 @@
-def init():
-    global uid
-    uid = ""
-    global database_url
-    database_url = 'https://osp-raporty.firebaseio.com/'
-    global database_auth_key
-    database_auth_key = '4jxwy5QOS3fItV8i69hEH15yRdBas0ps6oKNecFy'
-    global reports
-    reports = ""
--- pc-demo/MainMenu.py
+++
@@ -1,9 +0,0 @@
-from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
-with open('mainmenu.kv', encoding='utf8') as f:
-    Builder.load_string(f.read())
-
-
-class MainMenu(Screen):
-    pass
